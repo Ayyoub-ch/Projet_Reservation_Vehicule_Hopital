c89426bfe8ab7e3175962633ee0035dcf3f6e5b7
Ayyoub-Ch <ayyoub.chetouani59@gmail.com>
2026-03-05 12:37:38 +0100

    structure du projet reservation vehicule version solo

---
+++ App/Personne.java
@@ -0,0 +1,46 @@
+public class Personne {
+    // Attributs
+    private String matricule;
+    private String  nom;
+    private String telephone;
+    private Service unService;
+
+    public Personne(String matricule, String nom, String telephone) {
+        this.matricule = matricule;
+        this.nom = nom;
+        this.telephone = telephone;
+    }
+    // getters et setters
+
+    @Override
+    public String toString() {
+        return "Personne{" + "matricule=" + matricule + ", nom=" + nom + ", telephone=" + telephone + '}';
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    } 
+    
+    
+}
+
